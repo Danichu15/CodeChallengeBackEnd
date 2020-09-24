d44f2989aebaa8839fdd95fb9cc259ea0efd20e4
Daniel Liszewski <48332083+Danichu15@users.noreply.github.com>
2020-09-24 11:32:27 +0200

    Update Address.java

--- src/main/java/com/codeChallenge/demo/data/address/Address.java
+++ src/main/java/com/codeChallenge/demo/data/address/Address.java
@@ -9,7 +9,7 @@ import javax.persistence.Id;
 public class Address {
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String street;
 	private String state;
